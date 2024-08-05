09e946f8df054ef07d789d66ca22b69ed5bf709f
PrathameshBende <91463382+PrathameshBende@users.noreply.github.com>
2024-08-05 15:11:04 +0530

    Delete test/FruitDetector directory

--- test/FruitDetector/detection/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('upload_image/', views.upload_image, name='upload_image'),
-    path('chat/', views.chat, name='chat'),  # Add this line for chat handling
-]
